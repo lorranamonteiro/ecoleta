08d386de1f02f69623b5ea2faa4698b8a6cbc952
lorranamonteiro <lorranakeller@gmail.com>
2020-06-08 13:15:43 -0300

    Criação da pgn create-point, search-result

---
+++ scripts/intro.js
@@ -0,0 +1,64 @@
+//COMNENTS
+/*ocument.write("Hello")*/
+
+//Variáveis tipos de dados 
+/*et myvar = "he"
+const myconst = "he"
+
+document.write(myconst + myvar)*/
+
+//STRING
+/*Isso é uma string"
+'Isso também é um string'
+`Isso é uma string também`*/
+
+//Number 
+/*onst n1 = 1
+const n2 = 12
+document.write (n1 + n2)*/
+
+//boolean- True or False
+/*osnt bTrue = True
+const bFalse = False
+document.write (bTrue)*/
+
+//objetos possuem propriedades e funcionalidades
+/* const pessoa = {
+  altura: "1,88m",
+  idade: 24,
+  solteiro: true
+  correr() {
+    document.write("executar uma grande lógica de correr")
+  }  
+}
+pessoa.correr()*/
+
+//Array-vetores
+/* const colacaodebolinhas = ["blue", "green", 1 {nome: "my nome"}]
+
+documents.write(colecaodebolinhas[3].name*/
+
+// Funções - funcionalidades - atalhos - recurso de códigos
+
+//// Regitrar função
+/* funcion sayMyName(Name) {
+document.write(name)
+}*/
+
+//// executar
+/* sayMyName("Douglas")
+   sayMyName("Valeska")
+   sayMyNome("Kyan")*/
+
+// Codicionais
+/* const notafinal = 7
+if(notafinal < 5) {
+documente.write("reprovado")
+} else {
+document.write("aprovado")
+}*/
+
+//loop de repetições
+/*for(i=0; i<10; i++){  
+  document.write(`<p>${i}</p>`)
+}*/
